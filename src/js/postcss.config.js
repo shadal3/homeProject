b130057b95748b8127b1e9e17f85695dba959997
Igor <shadal490@gmail.com>
2020-02-01 16:01:53 +0200

    Update postcss.config.js file for autoprefixer, mqpacker, cssnano

---
+++ src/js/postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'), //  will use the data based on current browser popularity and property support to apply prefixes (-webkit-)
+        require('css-mqpacker'), //merge media things into one (media at parameter)
+        require('cssnano')({ //optimize and minify code for production environment
+            preset: [
+                'default', {
+                    discardComments: {
+                        removeAll: true,
+                    }
+                }
+            ]
+        })
+    ]
+};
