7e825cec97abdeb34010bd265afd756798805be4
Igor Kozlovski <igor.kozlovski@derivco.ee>
2021-03-13 12:06:00 +0200

    push initial code based on RX

--- src/main.js
+++ src/main.js
@@ -1,15 +1,55 @@
 // main.js
 
+require("regenerator-runtime"); // to fix await in case babel polyfills the code (polyfilling the code removes async/await native)
 
 //Required External Modules
 
-const express = require('express');
-const readline = require('readline')
+const Express = require('express');
+const Readline = require('readline');
+const Binance = require('node-binance-api');
+
+const Rx = require('@reactivex/rxjs');
 
 //App Variables
 
-const app = express();
+const app = Express();
 const port = "8080";
+
+const obs1 = new Rx.BehaviorSubject(false);
+const obs2 = new Rx.BehaviorSubject(true);
+const dummySubject = new Rx.BehaviorSubject(true);
+
+
+
+
+getApiKeys()
+  .switchMap(([apiKey, apiKeySecret]) => {
+    const binance = new Binance().options({
+      APIKEY: apiKey,
+      APISECRET: apiKeySecret
+    })
+    console.log(binance.time().then(time => console.log(time)));
+    
+    return new Rx.BehaviorSubject(true);
+  })
+  .subscribe((temp) => {
+    console.log(temp);
+  });
+
+
+/*const binance = new Promise((resolve, err) => {
+  getApiKeys().then((_) => {
+    const result = Binance().options(_);
+    resolve(result);
+  })
+})*/
+
+
+
+//Server Activation
+/*app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});*/
 
 
 /**
@@ -18,36 +58,38 @@ const port = "8080";
 
 //Routes Definitions
 
+//start App
 
+
+function getApiKeys() {
+  return dummySubject
+    .switchMap(() => {
+      return askQuestion("Please enter your API key: ")
+    })
+    .switchMap((apiKey) => {
+      const apiSecretKey = askQuestion("Please enter your secret API key: ")
+      return Rx.Observable.zip(Rx.Observable.of(apiKey), apiSecretKey);
+    });
+}
 
 function askQuestion(question) {
-    const rl = readline.createInterface({
+    const rl = Readline.createInterface({
         input: process.stdin,
         output: process.stdout
     })
-
-    return new Promise((resolve, err) => {
-        rl.question(question, (name) => {
-            console.log(`hey there ${name}`);
-            rl.close();
-            resolve(name);
-        })
-    }).then((data) => console.log(data))
-}
-async function f1() {
-    const ans = await askQuestion("HOW ARE YOU?");
+    const answerObs = new Rx.Subject();
+  
+    rl.question(question, (name) => {
+        rl.close();
+      answerObs.next(name);
+    })
+  
+    return answerObs;
 }
 
-f1();
-
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
-});
-
-//Server Activation
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-});
+});*/
 
 
 /*
